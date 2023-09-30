114eada495dad395e7987f02ef97b0d6dd510247
Pratap Singh Sisodia <48864550+prataps135@users.noreply.github.com>
2023-09-30 17:26:13 +0530

    jsx app added.

---
+++ React/jsx/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>My favourite fruits</h1>
+      <ul>
+        <li>Oranges</li>
+        <li>Grapes</li>
+        <li>Apple</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
